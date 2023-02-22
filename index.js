86bcf0e24c565aa08f553ee5f2451c00ffdca3f1
avinashkvar <k3076183@gmail.com>
2023-02-22 22:07:04 +0530

    practice

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const connect = require('./db/connect');
+const { UserModel } = require('./db/user');
 const app = express();
 
 app.get('/',async (req,res)=>{
     res.send('hello')
+})
+
+app.get('/users',async (req,res)=>{
+    try {
+        let users = await UserModel.find();
+        res.send(users)
+    } catch (error) {
+        res.send(error)
+    }
 })
 
 connect()
